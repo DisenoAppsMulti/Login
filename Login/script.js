0e62ee1eaee23fe23d970b4679580e3e1f9a80a2
Sebastian <142378175+BasAcor@users.noreply.github.com>
2024-05-30 22:38:42 -0600

    Add files via upload

---
+++ Login/script.js
@@ -0,0 +1,6 @@
+function Login(){
+	var U = document.getElementById("U").value;
+	var P = document.getElementById("P").value;
+
+	if(U == "Regina" && P == "123")window.location = "https://google.com";
+}
